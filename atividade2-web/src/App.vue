8e36ab21340abaaa891f11107a06756abbeae761
stefanne sousa <stessysousa2@gmail.com>
2020-09-10 21:40:42 -0300

    atividade2

---
+++ atividade2-web/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+   
+    <fastCloud msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import fastCloud from './components/fastCloud.vue'
+
+export default {
+  name: 'App',
+  components: {
+    fastCloud
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
